864267649f2ad09142ec37baff8146e181bd2920
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-07-30 15:24:13 -0400

    first

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "https://example.com",
+    }
+  });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
