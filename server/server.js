f9d0408666f23ed5074e9dfdb289a72eb89a824b
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-08-02 15:25:11 -0400

    inital server

--- server/server.js
+++ server/server.js
@@ -2,20 +2,29 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require("socket.io")(server, {
-    cors: {
-      origin: "https://example.com",
-    }
-  });
+const socketio = require("socket.io")
+
+corsOptions = {
+  cors: true,
+  origins: ["http://localhost:3000"],
+}
+const io = socketio(server, corsOptions)
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on('connect', (socket) => {
+  console.log('a user connected');
+  socket.on("disconnect", () => {
+    console.log("a disconnect")
+  })
+
+  socket.on("handshake", () => {
+    console.log("recieved")
+  })
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(4000, () => {
+  console.log('listening on *:4000');
 });
